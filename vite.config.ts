5b4b016dae10ee14a63ff5179df5ce1adb227d00
Rumpel <hades137747575@gmail.com>
2023-05-10 16:32:02 +0800

    build: 引入elementplus

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
+function AutoImport(arg0: { resolvers: any[]; }): import("vite").PluginOption {
+  throw new Error('Function not implemented.');
+}
+
+function ElementPlusResolver() {
+  throw new Error('Function not implemented.');
+}
+
+function Components(arg0: { resolvers: any[]; }): import("vite").PluginOption {
+  throw new Error('Function not implemented.');
+}
+
