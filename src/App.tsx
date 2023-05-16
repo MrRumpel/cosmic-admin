ce08d43219945503ba063277dc9ca5ebe108f2aa
Rumpel <hades137747575@gmail.com>
2023-05-16 16:56:18 +0800

    config:  🔧 降级ts 解决 elem+ 导入报错

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,34 @@ import { defineComponent, ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
 import logo from '@/assets/logo.svg';
+import { ElConfigProvider } from 'element-plus';
 
 export default defineComponent({
   name: 'App',
+  components: { ElConfigProvider },
   setup() {
-    const size = ref('small');
     const zIndex = ref(3000);
-    return () => (
-      <el-config-provider size={size.value} z-index={zIndex.value}>
-        <header>
-          <img alt='Vue logo' class='logo' src={logo} width='125' height='125' />
-          <div class='wrapper'>
-            <HelloWorld msg='You did it!' />
+    return { zIndex };
+  },
+  render() {
+    const { zIndex } = this;
+    return (
+      <>
+        <el-config-provider z-index={zIndex}>
+          <header>
+            <img alt='Vue logo' class='logo' src={logo} width='125' height='125' />
+            <div class='wrapper'>
+              <HelloWorld msg='You did it!' />
 
-            <nav>
-              <RouterLink to='/'>Home</RouterLink>
-              <RouterLink to='/about'>About</RouterLink>
-            </nav>
-          </div>
-        </header>
-        <RouterView />
-      </el-config-provider>
+              <nav>
+                <RouterLink to='/'>Home</RouterLink>
+                <RouterLink to='/about'>About</RouterLink>
+              </nav>
+            </div>
+          </header>
+          <RouterView />
+        </el-config-provider>
+      </>
     );
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
   ],
   css: {
     preprocessorOptions: {
