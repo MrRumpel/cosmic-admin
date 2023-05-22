8aa2845df42ae048983e32221c14b4f4a7ce8e90
Rumpel <hades137747575@gmail.com>
2023-05-22 10:59:12 +0800

    file:  📦  添加外部依赖

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   css: {
     preprocessorOptions: {
       scss: {
@@ -23,6 +20,24 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/basic-api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(new RegExp(`^/basic-api`), ''),
+        // only https
+        // secure: false
+      },
+      '/upload': {
+        target: 'http://localhost:3300/upload',
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(new RegExp(`^/upload`), ''),
+      },
     },
   },
 });
